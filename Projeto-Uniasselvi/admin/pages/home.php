13c42e5460b222eb68c4df3a284458dfd32b4273
Andre Panizza <andredeveloper45@gmail.com>
2025-07-02 21:06:55 -0300

    update this code

--- Projeto-Uniasselvi/admin/pages/home.php
+++ Projeto-Uniasselvi/admin/pages/home.php
@@ -1,5 +1,7 @@
 <div class="home_content">
-    <h1>                   
+    <h1>  
+        <!-- Quero que ela carregue o conteudo da home -->     
+         
         Seja bem vindo, <b><?= $_SESSION['userName'] ?></b> <?= TITLE; ?>
     </h1>
 </div>
